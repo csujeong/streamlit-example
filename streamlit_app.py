4d5e9c3062da653f5f0f1b66c73c6fce687876c9
csjeong <31301992+csujeong@users.noreply.github.com>
2023-09-04 08:01:21 +0900

    Update streamlit_gpt_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,6 @@ import streamlit as st
 """
 # Streamlit! 첫번째 테스트
 
-`/streamlit_app.py` 소스를 편집해서 이 앱을 원하는 대로 맞춤설정하세요 :heart:
-
-궁금한 점이 있으면 [설명서](https://docs.streamlit.io) 와 [community
-forums](https://discuss.streamlit.io) 을 확인해보세요~~^^
-
 다음은 단 몇 줄의 코드로 수행할 수 있는 작업의 예입니다.:
 """
 
@@ -36,3 +31,50 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+# =========================================================
+# env 파일을 사용하기 위한 라이브러리 import
+#from dotenv import load_dotenv
+#load_dotenv()
+
+# langchain, chat 모드 사용
+from langchain.chat_models import ChatOpenAI
+chat_model = ChatOpenAI()
+
+# Llama 사용
+# from langchain.llms import CTransformers
+# lim = CTransformers(
+#     model ="llama-2-7b-chat.ggmlv3.q6_K.bin",
+#     model_type = "llama"
+# )
+
+# streamlit으로 Frontend 만들기
+import streamlit as st
+from PIL import Image
+
+# 화면 상단 여백 제거
+st.write("<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True)
+
+# 웹페이지 제목
+st.title("AI & Poem")
+st.markdown("### Your Topics, Our AI Poems")
+
+# 그림 삽입
+col1, col2, col3 = st.columns([0.25, 0.5, 0.25])
+with col2:
+#    img = Image.open("AI-Poet.png")
+    img = Image.open("britychat.png")
+    to st.image(img)
+
+# 시 주제 입력 받음
+content = st.text_input("시의 주제를 제시해 주세요")
+
+# 시 작성
+if st.button('시 작성 요청'):
+    with st.spinner('시 작성 중... 잠시만 기다려 주세요'):
+        result  = chat_model.predict(content + "에 대한 시를 써주세요")
+        st.write(result)
+
+# 수익화
+from streamlit_extras.buy_me_a_coffee import button
+button(username="jakukyr", floating=True, width=221)
